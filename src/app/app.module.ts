f806b2f7610bcda3d4c4f4b54bce088304695580
almostcarol <carolinekcbn@gmail.com>
2021-12-01 19:51:00 -0300

    entrar/cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CadastrarComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
